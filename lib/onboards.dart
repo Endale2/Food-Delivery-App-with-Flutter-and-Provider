945eab9dd441f804d386a0dacd269ea579cc8f40
Endale2 <endale406@gmail.com>
2024-11-17 22:01:38 +0300

    updated

--- lib/onboards.dart
+++ lib/onboards.dart
@@ -6,14 +6,14 @@ class Onboard {
 List<Onboard> onboards = [
   Onboard(
       image: "lib/images/onboard_one.png",
-      text1: "Grab Your\n Delicious Food",
-      text2: "Delivery Food on Your Phone"),
+      text1: "Crave It?\nWe Deliver!",
+      text2: "Satisfy your cravings with fast delivery"),
   Onboard(
       image: "lib/images/onboard_two.png",
-      text1: "Grab Your\n Delicious Food",
-      text2: "Delivery Food on Your Phone"),
+      text1: "Fresh Flavors,\nRight to You",
+      text2: "Experience the best dishes at your doorstep"),
   Onboard(
       image: "lib/images/onboard_three.png",
-      text1: "Grab Your\n Delicious Food",
-      text2: "Delivery Food on Your Phone")
+      text1: "Fast & Reliable\nFood Delivery",
+      text2: "Get your favorite meals without the wait")
 ];
--- lib/pages/app_on_board_page.dart
+++ lib/pages/app_on_board_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/onboards.dart';
+import 'package:animate_do/animate_do.dart';
 
 class AppOnBoardPage extends StatefulWidget {
   const AppOnBoardPage({super.key});
@@ -21,7 +22,7 @@ class _AppOnBoardPageState extends State<AppOnBoardPage> {
           PageView.builder(
             onPageChanged: (value) {
               setState(() {
-                currentIndex = value; // Update currentIndex when page changes
+                currentIndex = value;
               });
             },
             itemCount: onboards.length,
@@ -32,36 +33,42 @@ class _AppOnBoardPageState extends State<AppOnBoardPage> {
                     top: -70,
                     left: 0,
                     right: 0,
-                    child: Image.asset(
-                      onboards[index].image,
-                      width: 600,
-                      height: 600,
-                      fit: BoxFit.contain,
+                    child: FadeInUp(
+                      delay: const Duration(milliseconds: 500),
+                      child: Image.asset(
+                        onboards[index].image,
+                        width: 600,
+                        height: 600,
+                        fit: BoxFit.contain,
+                      ),
                     ),
                   ),
                   Positioned(
                     top: MediaQuery.of(context).size.height / 1.9,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 25),
-                      child: Column(
-                        children: [
-                          Text(
-                            onboards[index].text1,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 50,
-                              fontWeight: FontWeight.bold,
+                    child: FadeInUp(
+                      delay: const Duration(milliseconds: 500),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Column(
+                          children: [
+                            Text(
+                              onboards[index].text1,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 50,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 20),
-                          Text(
-                            onboards[index].text2,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
+                            const SizedBox(height: 20),
+                            Text(
+                              onboards[index].text2,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -72,41 +79,47 @@ class _AppOnBoardPageState extends State<AppOnBoardPage> {
           Positioned(
             bottom: 170,
             left: 25,
-            child: Row(
-              children: [
-                ...List.generate(
-                  onboards.length,
-                  (index) => AnimatedContainer(
-                    duration: const Duration(milliseconds: 250),
-                    height: 5,
-                    width: 50,
-                    margin: const EdgeInsets.only(right: 10),
-                    decoration: BoxDecoration(
-                        color: currentIndex == index
-                            ? Colors.white
-                            : Colors.white.withOpacity(0.5),
-                        borderRadius: BorderRadius.circular(15)),
+            child: FadeInUp(
+              delay: const Duration(milliseconds: 500),
+              child: Row(
+                children: [
+                  ...List.generate(
+                    onboards.length,
+                    (index) => AnimatedContainer(
+                      duration: const Duration(milliseconds: 250),
+                      height: 5,
+                      width: 50,
+                      margin: const EdgeInsets.only(right: 10),
+                      decoration: BoxDecoration(
+                          color: currentIndex == index
+                              ? Colors.white
+                              : Colors.white.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(15)),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           Positioned(
               bottom: 30,
-              child: SizedBox(
-                height: 75,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: MaterialButton(
-                    onPressed: () {},
-                    color: Colors.orange,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    minWidth: MediaQuery.of(context).size.width - 50,
-                    child: const Center(
-                      child: Text(
-                        "Get Started",
-                        style: TextStyle(color: Colors.white, fontSize: 24),
+              child: FadeInUp(
+                delay: const Duration(milliseconds: 500),
+                child: SizedBox(
+                  height: 75,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: MaterialButton(
+                      onPressed: () {},
+                      color: Colors.orange,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      minWidth: MediaQuery.of(context).size.width - 50,
+                      child: const Center(
+                        child: Text(
+                          "Get Started",
+                          style: TextStyle(color: Colors.white, fontSize: 24),
+                        ),
                       ),
                     ),
                   ),
