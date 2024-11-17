0ebc27d2fc0865535f61131468eb0991faf16d7b
Endale2 <endale406@gmail.com>
2024-11-17 21:09:08 +0300

    initial commit

---
+++ lib/onboards.dart
@@ -0,0 +1,19 @@
+class Onboard {
+  final String image, text1, text2;
+  Onboard({required this.image, required this.text1, required this.text2});
+}
+
+List<Onboard> onboards = [
+  Onboard(
+      image: "lib/images/onboard_one.png",
+      text1: "Grab Your\n Delicious Food",
+      text2: "Delivery Food on Your Phone"),
+  Onboard(
+      image: "lib/images/onboard_two.png",
+      text1: "Grab Your\n Delicious Food",
+      text2: "Delivery Food on Your Phone"),
+  Onboard(
+      image: "lib/images/onboard_three.png",
+      text1: "Grab Your\n Delicious Food",
+      text2: "Delivery Food on Your Phone")
+];
---
+++ lib/pages/app_on_board_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/onboards.dart';
+
+class AppOnBoardPage extends StatefulWidget {
+  const AppOnBoardPage({super.key});
+
+  @override
+  _AppOnBoardPageState createState() => _AppOnBoardPageState();
+}
+
+class _AppOnBoardPageState extends State<AppOnBoardPage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          PageView.builder(
+            onPageChanged: (value) {
+              setState(() {
+                currentIndex = value; // Update currentIndex when page changes
+              });
+            },
+            itemCount: onboards.length,
+            itemBuilder: (context, index) {
+              return Stack(
+                children: [
+                  Positioned(
+                    top: -70,
+                    left: 0,
+                    right: 0,
+                    child: Image.asset(
+                      onboards[index].image,
+                      width: 600,
+                      height: 600,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  Positioned(
+                    top: MediaQuery.of(context).size.height / 1.9,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Column(
+                        children: [
+                          Text(
+                            onboards[index].text1,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Text(
+                            onboards[index].text2,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+          Positioned(
+            child: Row(
+              children: [
+                ...List.generate(
+                  onboards.length,
+                  (index) => AnimatedContainer(
+                    duration: const Duration(milliseconds: 250),
+                    height: 5,
+                    width: 50,
+                    margin: const EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                      color: currentIndex == index
+                          ? Colors.white
+                          : Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
